e1b18a783cc287d6a993f2305108471a8733b101
Abee0508 <abdullah.qamar137@gmail.com>
2025-09-26 23:09:02 +0500

    Clean install for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // ✅ Vercel / static deploy ke liye fix
+  base: "./", // Vercel / static deploy ke liye fix
   plugins: [react()],
   server: {
     proxy: {
-      '/birthday-final/send-questionnaire.php': 'http://localhost',
+      "/birthday-final/send-questionnaire.php": "http://localhost",
     },
   },
-})
+});
