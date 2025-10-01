48d5fa616e7037422f6c8a3925675b101ea48a54
Abee0508 <abdullah.qamar137@gmail.com>
2025-10-02 00:11:36 +0500

    fix: Configure Vercel for serverless functions and restore body-parser for dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import bodyParser from "body-parser";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load .env file based on `mode` in the current working directory.
   const env = loadEnv(mode, process.cwd(), "");
 
-  return { plugins: [react()] };
+  return {
+    plugins: [react()],
+    server: {
+      middlewares: [
+        bodyParser.json({ limit: "100mb" }) // For handling large base64 data
+      ]
+    }
+  };
 });
